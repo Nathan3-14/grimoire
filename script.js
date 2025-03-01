c227a8f204aecf2767b7aba927e28f92c8e43f58
Nathan3-14 <nathan.watson.172@gmail.com>
2025-03-01 22:32:29 +0000

    Added basic layout of new reminder system

--- script.js
+++ script.js
@@ -23,6 +23,11 @@ shuffle(default_names)
 const urls = {"imp": "https://wiki.bloodontheclocktower.com/images/5/5c/Icon_imp.png", "baron": "https://wiki.bloodontheclocktower.com/images/6/6d/Icon_baron.png", "scarletwoman": "https://wiki.bloodontheclocktower.com/images/1/13/Icon_scarletwoman.png", "spy": "https://wiki.bloodontheclocktower.com/images/5/54/Icon_spy.png", "poisoner": "https://wiki.bloodontheclocktower.com/images/b/b1/Icon_poisoner.png", "butler": "https://wiki.bloodontheclocktower.com/images/1/19/Icon_butler.png", "drunk": "https://wiki.bloodontheclocktower.com/images/4/4a/Icon_drunk.png", "recluse": "https://wiki.bloodontheclocktower.com/images/6/60/Icon_recluse.png", "saint": "https://wiki.bloodontheclocktower.com/images/c/c9/Icon_saint.png", "mayor": "https://wiki.bloodontheclocktower.com/images/a/a1/Icon_mayor.png", "soldier": "https://wiki.bloodontheclocktower.com/images/c/c3/Icon_soldier.png", "slayer": "https://wiki.bloodontheclocktower.com/images/d/d3/Icon_slayer.png", "virgin": "https://wiki.bloodontheclocktower.com/images/d/d3/Icon_virgin.png", "ravenkeeper": "https://wiki.bloodontheclocktower.com/images/e/ef/Icon_ravenkeeper.png", "monk": "https://wiki.bloodontheclocktower.com/images/7/7c/Icon_monk.png", "undertaker": "https://wiki.bloodontheclocktower.com/images/0/05/Icon_undertaker.png", "fortuneteller": "https://wiki.bloodontheclocktower.com/images/9/97/Icon_fortuneteller.png", "empath": "https://wiki.bloodontheclocktower.com/images/1/13/Icon_empath.png", "chef": "https://wiki.bloodontheclocktower.com/images/d/d5/Icon_chef.png", "investigator": "https://wiki.bloodontheclocktower.com/images/a/ad/Icon_investigator.png", "librarian": "https://wiki.bloodontheclocktower.com/images/e/e0/Icon_librarian.png", "washerwoman": "https://wiki.bloodontheclocktower.com/images/8/85/Icon_washerwoman.png"}
 const reminders = {"imp.dead": "Dead", "drunk.drunk": "Drunk", "butler.master": "Master", "poisoner.poisoned": "Poisoned", "slayer.noability": "No Ability", "virgin.noability": "No Ability", "monk.protected": "Protected", "undertaker.executed": "Executed", "fortuneteller.redherring": "Red Herring", "investigator.wrong": "Wrong", "investigator.minion": "Minion", "librarian.wrong": "Wrong", "librarian.outsider": "Outsider", "washerwoman.wrong": "Wrong", "washerwoman.townsfolk": "Townsfolk"}
 
+var setup_reminders = () => {
+    //TODO DO SOMETHING HERE TODO//
+
+}
+
 var setup = (character_list) => {
     shuffle(character_list)
     const template = document.getElementById("character-template")
@@ -44,16 +49,8 @@ var setup = (character_list) => {
             player_modification_dialog.querySelector("dialog > form > h2").innerHTML = default_names[n-1]
         }
 
-        // if (current_character == "slayer") {
-        //     clone.querySelector("li").id = "a"
-        // }
         
         document.getElementsByClassName("radial")[0].appendChild(clone)
-        // if (current_character == "slayer") {
-        //     add_reminder("a", "drunk.drunk")
-        //     add_reminder("a", "slayer.noability")
-        //     add_reminder("a", "investigator.wrong")
-        // }
         
         var style_adjust_character = document.createElement("style")
         style_adjust_character.innerHTML = adjust_css_character.replaceAll("{{rotate}}", rotate).replaceAll("{{n}}", n)
@@ -66,6 +63,8 @@ var setup = (character_list) => {
         document.getElementById("radial-grid-adjust").appendChild(style_adjust_reminders_2)
         rotate = rotate + rotate_diff //make next token be rotated
     }
+    
+    setup_reminders()
 }
 
 var add_reminder = (player_name, reminder_id) => {
@@ -81,9 +80,8 @@ var add_reminder = (player_name, reminder_id) => {
     document.querySelector(`#${player_name} .reminders`).appendChild(clone)
 }
 
-var dialog_add_reminder = () => {
+var dialog_add_reminder = (reminder_id) => {
     var player_name = player_modification_dialog.querySelector("form > h2").innerHTML
-    var reminder_id = player_modification_dialog.querySelector("form > #reminder-input").value
     add_reminder(player_name, reminder_id)
     player_modification_dialog.querySelector("#dialog-extrainfo").innerHTML = ""
 }
