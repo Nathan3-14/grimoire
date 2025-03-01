a152209849909ab0695f7e387c80ed662568f466
Nathan3-14 <nathan.watson.172@gmail.com>
2025-03-01 18:22:44 +0000

    Moved everything

--- scripts/geturls.py
+++ scripts/geturls.py
@@ -24,20 +24,7 @@ for character in characters:
     dirs = result.hexdigest()[:2]
     dir_path = f"{dirs[0]}/{dirs}"
     url = f"https://wiki.bloodontheclocktower.com/images/{dir_path}/{filename}"
-    img_tag = f"<div class=\"character\">\n<img class=\"icon\" src={url}>\n</div>"
-
-    img_tag = open("character.html", "r").read().replace("{{url}}", url).replace("{{character}}", character.capitalize())
-    
-    to_out += img_tag
     urls[character] = url
     print(url)
-    
-    """
-    <div class="reminder">
-        <img src="https://wiki.bloodontheclocktower.com/images/4/4a/Icon_drunk.png" width="64" height="64">
-        <p>Drunk</p>
-    </div>
-    """
 
-# open("out.txt", "w").write(to_out)
-json.dump(urls, open("urls.json", "w"))
+json.dump(urls, open("data/urls.json", "w"))
