d1474a13c2cdeb78a4c1777aad88f787f465758c
Nathan3-14 <nathan.watson.172@gmail.com>
2025-03-02 17:19:18 +0000

    Added ability texts Changed multiword names to have _

--- scripts/geturls.py
+++ scripts/geturls.py
@@ -4,9 +4,9 @@ from typing import Dict
 
 characters = [
     "imp",
-    "baron", "scarletwoman", "spy", "poisoner",
+    "baron", "scarlet_woman", "spy", "poisoner",
     "butler", "drunk", "recluse", "saint",
-    "mayor", "soldier", "slayer", "virgin", "ravenkeeper", "monk", "undertaker", "fortuneteller", "empath", "chef", "investigator", "librarian", "washerwoman"
+    "mayor", "soldier", "slayer", "virgin", "ravenkeeper", "monk", "undertaker", "fortune_teller", "empath", "chef", "investigator", "librarian", "washerwoman"
 ]
 t_characters = [
     "imp",
@@ -19,7 +19,7 @@ urls: Dict[str, str] = {}
 to_out = ""
 
 for character in characters:
-    filename = f"Icon_{character}.png"
+    filename = f"Icon_{character.replace('_','')}.png"
     result = hashlib.md5(filename.encode())
     dirs = result.hexdigest()[:2]
     dir_path = f"{dirs[0]}/{dirs}"
